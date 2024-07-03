bde7fcde54ca7efc1e9b73b9201ecc0bc67564e1
jyoh <ojy1018@gmail.com>
2024-07-03 05:42:02 +0000

    빌드 시 발생하는 에러 수정(API_URL)

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
-import { API_URL } from "../app/(home)/page";
 import styles from "../styles/movie-videos.module.css"
+import { API_URL } from "./api-path";
 
 async function getVideos(id: string){
     // await new Promise(resolve => setTimeout(resolve, 3000));
